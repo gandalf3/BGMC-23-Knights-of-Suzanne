7145d9c02f5f5fe6a28c59f1b2deaf66425de551
gandalf3 <zzyxpaw@gmail.com>
2017-01-25 21:51:57 -0800

    initial commit; very basic movement implementation

---
+++ Player.py
@@ -0,0 +1,46 @@
+import bge
+from mathutils import Vector
+
+
+class Player(bge.types.KX_GameObject):
+    def __init__(self, own):
+        self.player_control = False
+        self.inertia = .1
+        self.speed = 1
+        self.speedfac = .05
+        self.direction = 1
+        
+    def move(self):
+        if self.sensors["Left"].positive:
+            self.speed = 1
+            self.direction = -1
+        if self.sensors["Right"].positive:
+            self.speed = 1
+            self.direction = 1
+        
+        self.worldPosition.x += self.speed*self.direction*self.speedfac
+        self.speed *= self.inertia
+        
+        if not self.children["PlayerVisual"].isPlayingAction():
+            self.children["PlayerVisual"].playAction("PlayerWalkcycle", 1, 30, play_mode=bge.logic.KX_ACTION_MODE_LOOP, speed=2)
+        if self.speed <= .01:
+            self.children["PlayerVisual"].stopAction()
+
+    def look(self):
+        self.alignAxisToVect((0,0,1), 2, 1)
+        self.alignAxisToVect((-self.direction,0,0), 1, .4)
+            
+    
+    def main(self):
+        self.move()
+        self.look()
+        
+        
+def run(cont):
+    own = cont.owner
+    
+    if "player_init" not in own:
+        own["player_init"] = True
+        own = Player(own)
+        
+    own.main()
